df18d1b563e55e7ba59d2981c1e2aecec716db39
liujunting <1446177808@qq.com>
2020-05-17 18:27:15 +0800

    one

---
+++ ECT/app/src/main/java/com/hbsd/rjxy/ect/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hbsd.rjxy.ect.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hbsd.rjxy.ect.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
